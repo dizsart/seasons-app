7d96341b76e4e0be4852c4ab6958ad9cffb0cb62
dizsart <darogadibia@gmail.com>
2020-05-13 14:44:30 +0100

    refactor: make changes to the seasonDisplay code for better readability

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+    summer : {
+        text : "Let's hit the beach!",
+        icon : "sun icon"
+    },
+    winter : {
+        text : "Burr, it is chilly!",
+        icon : "snowflake icon"
+    }
+};
+
 const getSeason = (lat, month) => {
 if (month > 2 && month < 9){
     return lat > 0 ? 'summer' : 'winter';
@@ -10,10 +21,14 @@ else{
 }
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
+    const icon = season === 'winter'? seasonConfig.winter.icon : seasonConfig.summer.icon;
+    const text = season === 'winter'? seasonConfig.winter.text: seasonConfig.summer.text;
     return (
         <div>
         <div>Season Display</div>
-        <div>{season === 'winter'?'Burr, it is chilly' : 'Let\'s hit the beach'}</div>
+        <i className = {icon}/>
+        <div>{text}</div>
+        <i className = {icon}/>
         </div>
     )
 };
